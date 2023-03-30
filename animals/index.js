a0b40b30b5ee2e9fcb418758024681b51e11680b
akavaliova <kovaleva89alena@gmail.com>
2023-03-30 10:04:02 +0200

    add js function for active slide

--- animals/index.js
+++ animals/index.js
@@ -1,13 +1,23 @@
-const slides = document.querySelectorAll('.slide');
-for (const slide of slides) {
-    slide.addEventListener('click', () =>{
-        clearActiveClasses()
-        slide.classList.add('active')
-    })
-}
+function slidesPluginActive(activeSlide = 1) {
 
-function clearActiveClasses(){
-    slides.forEach((slide) =>{
-        slide.classList.remove('active')
-    })
+  const slides = document.querySelectorAll(".slide");
+
+  slides[activeSlide].classList.add('active');
+
+
+  for (const slide of slides) {
+    slide.addEventListener("click", () => {
+      clearActiveClasses();
+      slide.classList.add("active");
+    });
+  }
+
+  function clearActiveClasses() {
+    slides.forEach((slide) => {
+      slide.classList.remove("active");
+    });
+  }
 }
+slidesPluginActive();
+
+
